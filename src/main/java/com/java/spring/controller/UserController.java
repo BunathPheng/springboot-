15a22f0b80980520c25bf3dc0cf697ba347ef37a
Pheng Bunath <bunathpheng4@gmail.com>
2025-11-19 09:28:28 +0700

    Config deployment

--- src/main/java/com/java/spring/controller/UserController.java
+++ src/main/java/com/java/spring/controller/UserController.java
@@ -272,4 +272,8 @@ public class UserController {
     public String getStringlesOTSSSSDDDDDDLLLDSIUOOOOOOIUUUUDDDDKOOOpkillllIOPPPLolologigoGUGUjijigogoloiiiyllyuyipopopopoooooooooiodslokloveuuuppppops() {
         return "hello This API is working !";
     }
+    @GetMapping("/lynasok")
+    public String getStringlesOTSSSSDDDDDDLLLDSIUOOOOOOIUUUUDDDDKOOOpkillllIOPPPLolologigoGUGUjijigogoloiiiyllyuyipopopopoooooooooiodslokloveuuuppppopssok() {
+        return "hello This API is working !";
+    }
 }
